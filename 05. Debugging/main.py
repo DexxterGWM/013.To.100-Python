84b4b16f8c697d097c2a8f233cb3c12ac7b6cf90
DexxterGWM <99261879+DexxterGWM@users.noreply.github.com>
2023-07-10 19:21:41 -0300

    main.py File

---
+++ 05. Debugging/main.py
@@ -0,0 +1,17 @@
+##################### DEBUGGING #####################
+
+# Print is Your Friend
+
+pages = 0
+word_per_page = 0
+
+pages = int(input(' Number of pages: '))
+
+# word_per_page == int(input(' Number of words per page: '))
+word_per_page = int(input(' Number of words per page: '))
+
+total_words = pages * word_per_page
+
+print(f' peges = {pages}')
+print(f' word_per_page = {word_per_page}')
+print(total_words)
