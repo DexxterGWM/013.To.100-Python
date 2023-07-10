1f8fe33294562a7248dcf19212b1f051d3ceebb7
DexxterGWM <99261879+DexxterGWM@users.noreply.github.com>
2023-07-10 19:18:50 -0300

    main.py File

---
+++ 02. Debugging/main.py
@@ -0,0 +1,12 @@
+##################### DEBUGGING #####################
+
+# Reproduce the Bug
+
+from random import randint
+
+dice_imgs = ['❶', '❷', '❸', '❹', '❺', '❻']
+
+# dice_num = randint(1, 6)
+dice_num = randint(0, 5)
+
+print(dice_imgs[dice_num])
