77eec5d91c607e274b85440ee547d008166585cd
DexxterGWM <99261879+DexxterGWM@users.noreply.github.com>
2023-07-10 19:20:30 -0300

    main.py File

---
+++ 04. Debugging/main.py
@@ -0,0 +1,10 @@
+##################### DEBUGGING #####################
+
+# Fix the Errors
+
+# age = input(' How old are you? ')
+age = int(input(' How old are you? '))
+
+if age > 18:
+    # print(' You can drive at age {age}.')
+    print(f' You can drive at age {age}.')
